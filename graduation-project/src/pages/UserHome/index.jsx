f09ec9a96fb9d5786aa27beec133b4058363ffd7
罗鹏 <luopeng1@yonyou.com>
2023-09-12 10:13:44 +0800

    修改背景图名字，删除无用项目图片； 首页增加背景图，登录注册界面修改背景图； 创建注册界面，修改登录和注册界面的css文件

--- graduation-project/src/pages/UserHome/index.jsx
+++ graduation-project/src/pages/UserHome/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+ 
+import "./index.css"
 
 export default function UserHome() {
   return (
-    <div>
+    <div className='userHomeDiv'> 
       这是商城首页
     </div>
   )
--- graduation-project/src/pages/UserRegister/index.jsx
+++ graduation-project/src/pages/UserRegister/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import { Col, Row } from "antd"
+
+import "./index.css"
+
 export default function Userregister() {
   return (
-    <div>
-      这是注册界面
-    </div>
+    <div className='userRegisterBackground'>
+    <Row>
+      <Col span={6}></Col>
+      <Col span={6}>
+        <div className='useRegisterDivLeft'>
+          <span className='userRegisterSpanText'>欢迎使用!!!<br />React二手商城</span>
+        </div>
+      </Col>
+      <Col span={9} offset={2}>
+        <div className='userRegisterDivRight'></div>
+          这是注册Form单
+      </Col>
+    </Row>
+  </div>
   )
 }
+
